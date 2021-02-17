17a1c5ee03d0468b5d66028228a352148dc38cf1
HannahStrimpel <h4rescue@gmail.com>
2021-02-17 14:45:56 -0500

    Not yet finished pseudo code for drivetrain.

---
+++ 2021-Robot/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,28 @@
+package frc.robot;
+
+public class Drivetrain {
+    
+    // TalonFX right motor
+    // TalonFX left motor
+    // TalonFX slave right motor
+    // TalonFX slave left motor
+
+    // Right Encoder
+    // Left Encoder
+
+    // Right Solenoid
+    // Left Solenoid
+
+    // enum for storing the possible gears that are used for the drivetrain
+
+
+
+    public Drivetrain(){
+        // Instantiate TalonFX motors
+
+        // Instantiate left and right pistons (solenoids)
+
+        // Set gears initially to unknown, so gears can be switch to high or low at first
+    }
+
+}
