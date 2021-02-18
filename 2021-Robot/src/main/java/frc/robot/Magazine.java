39050d14550bbdfaf2795dcbf2fe5a9ad944a66c
OwenM14 <owenmorrow@comcast.net>
2021-02-18 14:47:27 -0500

    added psudocode for the magazine class

---
+++ 2021-Robot/src/main/java/frc/robot/Magazine.java
@@ -0,0 +1,34 @@
+/**
+ *  Psudocode for the magazine class.
+ *  The class will require variables for motors as well as their RobotMap
+ *  information. This class will also contain variables for sensors so we can index the power cells.
+ */
+   
+public class Magazine {
+
+// declare motor
+
+// declare intake sensor
+// declare outtake sensor
+
+    /**
+     * Instantiate the prior declared variables
+     */
+    public Magazine(){
+        
+    } 
+
+    /**
+     * Sets the magazine to run based on on a set motor speed
+     */
+    public void RunMagazine(){
+        
+    }
+
+    /**
+     * Uses an if statment and sensor information to count the Power cells stored 
+     */
+    public void CellCount(){
+        
+    }
+}
