3d9d7c11f2216c9a12e1e930f8719af08debbdad
OwenM14 <owenmorrow@comcast.net>
2021-02-25 09:52:46 -0500

    Created basic launcher class based on previous pseudo code. Also added the imports used from last year into the class

--- 2021-Robot/src/main/java/frc/robot/Magazine.java
+++ 2021-Robot/src/main/java/frc/robot/Magazine.java
@@ -1,34 +1,35 @@
-/**
- *  Psudocode for the magazine class.
- *  The class will require variables for motors as well as their RobotMap
- *  information. This class will also contain variables for sensors so we can index the power cells.
- */
-   
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.SensorCollection;
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;  
+
 public class Magazine {
 
-// declare motor
-
-// declare intake sensor
-// declare outtake sensor
+    //Declare motor controller for the magazine
+    private TalonSRX m_motor;
 
     /**
-     * Instantiate the prior declared variables
+     * Constructor for magazine objects
+     * @param motor The motor that runs the magazine
+     */
+    public Magazine(TalonSRX motor){
+        m_motor = motor;
+    } 
+
+    /**
+     * Vertical constuctor for instantiation
      */
     public Magazine(){
-        
-    } 
+        m_motor = new TalonSRX(RobotMap.MAGAZINE_MOTOR_PORT);
+    }
 
     /**
      * Sets the magazine to run based on on a set motor speed
      */
-    public void RunMagazine(){
-        
+    public void RunMagazine(double speed){
+        m_motor.set(ControlMode.PercentOutput, speed);
     }
 
-    /**
-     * Uses an if statment and sensor information to count the Power cells stored 
-     */
-    public void CellCount(){
-        
-    }
 }
